8b3b281256de6e0b9a1ec224c38ee677f839fc3d
RubinaKhan <rubinatazak@gmail.com>
2023-12-11 19:25:57 +0530

    createdproduct  and customer data page

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import { themeSettings } from "theme";
 import Dashboard  from "scenes/dashboard";
 import Layout from "scenes/layout";
 import Products from "scenes/Products";
+import Customers from "scenes/customers"
 
 function App() {
 
@@ -23,6 +24,7 @@ function App() {
             <Route path="/" element = {<Navigate to ="dashboard" replace />} />
             <Route path="/dashboard" element = {<Dashboard/>} />
             <Route path="/products" element = {<Products/>} />
+            <Route path="/customers" element = {<Customers/>} />
             </Route>
         </Routes>       
       </ThemeProvider>
